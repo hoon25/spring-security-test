fc1b9c45f3be18a369c12ffe4b32f96f362b1042
hoon25 <cchoon95@hanmail.net>
2023-12-30 20:41:42 +0900

    feat(security): UserDetailsService

--- src/main/java/io/security/basic/config/SecurityConfig.java
+++ src/main/java/io/security/basic/config/SecurityConfig.java
@@ -1,27 +1,32 @@
 package io.security.basic.config;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    UserDetailsService userDetailsService;
-
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/admin/pay").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') or hasRole('SYS')"))
                         .anyRequest()
-                        .authenticated())
+                        .authenticated()
+                )
                 .formLogin((formLogin) -> formLogin
 //                        .loginPage("/loginPage")
                                 .defaultSuccessUrl("/")
@@ -55,7 +60,6 @@ public class SecurityConfig {
                         .rememberMeParameter("remember")
                         .tokenValiditySeconds(3600)
                         .alwaysRemember(false)
-                        .userDetailsService(userDetailsService)
                 ).sessionManagement((sessionManagement) -> sessionManagement
                         .sessionFixation().changeSessionId()
                         .maximumSessions(1)
@@ -63,5 +67,26 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, sys, admin);
     }
 }

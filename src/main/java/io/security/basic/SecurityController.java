a621622f58ef5af58b7aecdbb63fa48778c3696d
hoon25 <cchoon95@hanmail.net>
2024-01-02 07:40:56 +0900

    feat(security): SecurityContextHolder

--- src/main/java/io/security/basic/SecurityController.java
+++ src/main/java/io/security/basic/SecurityController.java
@@ -1,5 +1,10 @@
 package io.security.basic;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+        System.out.println(authentication);
+        System.out.println(authentication1);
         return "home";
     }
 
@@ -42,4 +52,14 @@ public class SecurityController {
         return "Access is denied";
     }
 
+    @GetMapping("/thread")
+    public String thread() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("authentication = " + authentication);
+        new Thread(() -> {
+            Authentication threadAuthentication = SecurityContextHolder.getContext().getAuthentication();
+            System.out.println("threadAuthentication = " + threadAuthentication);
+        }).start();
+        return "thread";
+    }
 }
--- src/main/java/io/security/basic/config/SecurityConfig.java
+++ src/main/java/io/security/basic/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -81,6 +82,7 @@ public class SecurityConfig {
                         })
                 );
 
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
         return http.build();
     }
 
